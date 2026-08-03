7083ce1b9cf2ac5fc73ab58bd33ffb1137b82843
Matheus Pereira de Andrade <rm99541@estudante.fieb.edu.br>
2026-08-03 11:47:32 -0300

    eu sou mt sozinho pqp

--- src/back-end/pizzarianapoli/src/main/java/com/itb/in2em/pizzarianapoli/controller/ProdutoController.java
+++ src/back-end/pizzarianapoli/src/main/java/com/itb/in2em/pizzarianapoli/controller/ProdutoController.java
@@ -14,6 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itb.in2em.pizzarianapoli.model.entity.Produto;
 import com.itb.in2em.pizzarianapoli.model.services.ProdutoService;
 
+// A seguir annotation do (Spring web MVC) dependências: "spring-boot=-starter-webmvc (pom.xml
+
+// Annotation de classe
+
+// @Controller: Sistema web (Sites em geral) - back-end + Front-end
+//@RestController: Api (apenas em api's) - Apenas em back-end
+
+//Annotation de métodos 
+
+//@ GetMapping: Utilizado para "buscar" dados na API (Somente Pesquisa)
+//@ PostMapping: Utilizado para "enviar" dados na API (Cadastros)
+//@ PutMapping: Utilizado para "atualizar" todos os dados na API ()
+//@ DeleteMapping: Utilizado para "excluir" dados na API ()
+//@ PatchMapping: Utilizado para "atualizar parcialmente" dados na API, exemplo: mudar o status de um produto
+
+//ResponseEntity: Controla a resposta HTTP completa de um API, permitindo definir o corpo (body), o código de status (200, 201, 400 etc.)
+//                e os cabeçalhos (headers)
+
+
 @RestController
 @RequestMapping("/api/v1/produtos")
 public class ProdutoController {
